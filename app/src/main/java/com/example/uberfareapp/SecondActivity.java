d0b47fc67fa425e3cb09669bf87d28df8e6d1064
Lac <lacstoll@gmail.com>
2023-03-02 14:30:02 -0600

    Add images

--- app/src/main/java/com/example/uberfareapp/SecondActivity.java
+++ app/src/main/java/com/example/uberfareapp/SecondActivity.java
@@ -5,13 +5,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
     TextView vehicleTxt, distanceTxt, totalTxt;
-
+    ImageView vehicleImg;
     Button RequestRideBtn, goBackButton;
 
     SharedPreferences sharedPreferences;
@@ -25,6 +26,7 @@ public class SecondActivity extends AppCompatActivity {
         vehicleTxt = findViewById(R.id.vehicleTxt);
         distanceTxt = findViewById(R.id.distanceTxt);
         totalTxt = findViewById(R.id.totalTxt);
+        vehicleImg = findViewById(R.id.vehicleImg);
         RequestRideBtn = findViewById(R.id.RequestRideBtn);
         goBackButton = findViewById(R.id.goBackButton);
 
@@ -35,17 +37,30 @@ public class SecondActivity extends AppCompatActivity {
 
         double fare = 3.0 + (3.25 * distance);
 
+//        switch (vehicle) {
+//            case "Smart Car":
+//                fare += 2.0;
+//                break;
+//            case "Minivan":
+//                fare += 5.0;
+//                break;
+//            case "Sedan":
+//                fare += 0;
+//                break;
         switch (vehicle) {
             case "Smart Car":
                 fare += 2.0;
+                vehicleImg.setImageResource(R.drawable.smart_car);
+                break;
+            case "Sedan":
+                vehicleImg.setImageResource(R.drawable.sedan);
                 break;
             case "Minivan":
                 fare += 5.0;
-                break;
-            case "Sedan":
-                fare += 0;
+                vehicleImg.setImageResource(R.drawable.minivan);
                 break;
         }
+
         vehicleTxt.setText(String.format("Vehicle: " + vehicle));
         distanceTxt.setText(String.format("Distance: " + distance + " miles"));
         totalTxt.setText(String.format("Total: $%.2f", fare));
